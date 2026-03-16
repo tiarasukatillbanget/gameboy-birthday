f45a17e5ab5bedd93c5ae8cb595617c117a51706
tiarasukatillbanget <anggrekputritiara@gmail.com>
2026-03-16 13:57:35 +0000

    add new image

--- script.js
+++ script.js
@@ -223,15 +223,15 @@ function startTypewriter() {
     const messageContent = document.querySelector('.message-content');
     if (!messageContent) return;
     
-    const fullMessage = `Hi,
+    const fullMessage = `HALOHALOOO,
 
-Happy Birthday!
+How are you today kak ryoo?
 
-Hari ini aku pengen kamu ngerasain semua hal positif dan keajaiban yang cuma bisa didapetin kalo kamu ada di dunia ini. Semoga segala keinginanmu tercapai, apalagi yang kocak-kocak dan gak biasa, karena kamu tuh unik banget! Aku selalu percaya kalau kamu bisa melewati semua tantangan dengan kekuatan dan semangat yang luar biasa.
+Hari ini ara pengen ngambil langkah pertama untuk hubungan kita yang lebih jauh hihi. But first of all ara pengen kamu tahu seberapa ara sayanggg banget, cintaa banget, respect bangett sama kamu. soalnya kamu keren banget wadehelll, pinter MTK, rajin, jago gitar,
 
-Terima kasih udah jadi bagian hidup aku yang paling berharga. Kamu bener-bener bikin hari-hari aku jadi lebih berarti dan penuh warna. Semoga di tahun yang baru ini, kamu makin bahagia, makin sukses, dan tentunya makin cantik (walaupun udah cantik banget sih!).
+jago masak, cat lover, cantik, dewasa, omg ure so perfect in my eyes... i really want to love u with all of my heart, i want to put all of my efforts, my time, my dedication, to you.
 
-I love you so much! 💕`;
+So... can i be your girlfriend, sweetie?`;
     
     // Clear content and start fresh
     messageContent.innerHTML = '';
@@ -266,7 +266,7 @@ function skipTypewriter() {
         clearInterval(typewriterInterval);
         const messageContent = document.querySelector('.message-content');
         if (messageContent) {
-            const fullMessage = `Hi Cel,<br><br>Happy Birthday!<br><br>Hari ini aku pengen kamu ngerasain semua hal positif dan keajaiban yang cuma bisa didapetin kalo kamu ada di dunia ini. Semoga segala keinginanmu tercapai, apalagi yang kocak-kocak dan gak biasa, karena kamu tuh unik banget! Aku selalu percaya kalau kamu bisa melewati semua tantangan dengan kekuatan dan semangat yang luar biasa.<br><br>Terima kasih udah jadi bagian hidup aku yang paling berharga. Kamu bener-bener bikin hari-hari aku jadi lebih berarti dan penuh warna. Semoga di tahun yang baru ini, kamu makin bahagia, makin sukses, dan tentunya makin cantik (walaupun udah cantik banget sih!).<br><br>I love you so much! 💕`;
+            const fullMessage = `HALOHALOOO,<br><br>How are you today kak ryoo?<br><br>Hari ini ara pengen ngambil langkah pertama untuk hubungan kita yang lebih jauh hihi. But first of all ara pengen kamu tahu seberapa ara sayanggg banget, cintaa banget, respect bangett sama kamu. soalnya kamu keren banget wadehelll, pinter MTK, rajin, jago gitar,<br><br>jago masak, cat lover, cantik, dewasa, omg ure so perfect in my eyes... i really want to love u with all of my heart, i want to put all of my efforts, my time, my dedication, to you.<br><br>So... can i be your girlfriend, sweetie?`;
             messageContent.innerHTML = fullMessage;
             isTyping = false;
             messageContent.scrollTop = messageContent.scrollHeight;
@@ -378,7 +378,7 @@ function startPhotoShow() {
             <div class="photo-frames-container">
                 ${framesHTML}
             </div>
-            <div class="photo-strip-footer">💕 BIRTHDAY MEMORIES 💕</div>
+            <div class="photo-strip-footer">💕 OUR MEMORIES 💕</div>
         </div>
         <div class="scroll-indicator">⬇ Scroll Down ⬇</div>
     `;
